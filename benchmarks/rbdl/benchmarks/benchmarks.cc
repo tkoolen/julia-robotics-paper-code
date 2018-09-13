e9dc950d8361a0d839965b3fedefca77cf219206
Twan Koolen <koolen.twan@gmail.com>
2018-09-13 17:04:28 -0400

    Store URDF locally, RBDL benchmarks.

---
+++ benchmarks/rbdl/benchmarks/benchmarks.cc
@@ -0,0 +1,45 @@
+/*
+ * RBDL - Rigid Body Dynamics Library
+ * Copyright (c) 2011-2016 Martin Felis <martin@fysx.org>
+ *
+ * Licensed under the zlib license. See LICENSE for more details.
+ */
+
+#include <iostream>
+
+#include <rbdl/rbdl.h>
+
+#ifndef RBDL_BUILD_ADDON_URDFREADER
+	#error "Error: RBDL addon URDFReader not enabled."
+#endif
+
+#include <rbdl/addons/urdfreader/urdfreader.h>
+
+using namespace RigidBodyDynamics;
+using namespace RigidBodyDynamics::Math;
+
+int main (int argc, char* argv[]) {
+	rbdl_check_api_version (RBDL_API_VERSION);
+
+	Model* model = new Model();
+
+	if (!Addons::URDFReadFromFile ("./samplemodel.urdf", model, false)) {
+		std::cerr << "Error loading model ./samplemodel.urdf" << std::endl;
+		abort();
+	}
+
+	VectorNd Q = VectorNd::Zero (model->dof_count);
+	VectorNd QDot = VectorNd::Zero (model->dof_count);
+	VectorNd Tau = VectorNd::Zero (model->dof_count);
+	VectorNd QDDot = VectorNd::Zero (model->dof_count);
+
+ 	ForwardDynamics (*model, Q, QDot, Tau, QDDot);
+
+	std::cout << Q.transpose() << std::endl;
+	std::cout << QDDot.transpose() << std::endl;
+
+	delete model;
+
+ 	return 0;
+}
+
---
+++ benchmarks/rbdl/run.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+mkdir -p build
+cd build/
+cmake -D CMAKE_BUILD_TYPE=Release -D RBDL_BUILD_ADDON_URDFREADER=ON -D RBDL_BUILD_ADDON_BENCHMARK=ON ../rbdl-rbdl-0879ee8c548a
+make -j
+addons/benchmark/benchmark --floating-base ../../atlas.urdf
