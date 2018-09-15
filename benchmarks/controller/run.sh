8e6d5cd7c603000b878022b5f2992f29d5cbd177
Robin Deits <robin.deits@gmail.com>
2018-09-15 14:09:48 -0400

    clean up child processes on exit

--- benchmarks/controller/run.sh
+++ benchmarks/controller/run.sh
@@ -2,3 +2,6 @@
 julia --project --color=yes -e 'using Pkg; pkg"precompile"'
 julia --project -O3 --check-bounds=no --color=yes -e 'using HumanoidLCMSim; AtlasControl.run()' &
 julia --project -O3 --check-bounds=no --color=yes -e 'using HumanoidLCMSim; AtlasSim.run(final_time=10.0)'
+
+# Clean up child process on exit: https://stackoverflow.com/a/2173421/641846
+trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
