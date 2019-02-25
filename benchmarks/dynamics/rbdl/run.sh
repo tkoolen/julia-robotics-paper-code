77da95f110cd77ec6e020ece0367482b8451700c
Twan Koolen <koolen.twan@gmail.com>
2019-02-25 17:15:28 -0500

    RBDL benchmark update

--- benchmarks/dynamics/rbdl/run.sh
+++ benchmarks/dynamics/rbdl/run.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 mkdir -p build
 cd build/
+unset ROS_ROOT
 cmake -D CMAKE_BUILD_TYPE=Release -D RBDL_BUILD_ADDON_URDFREADER=ON -D RBDL_BUILD_ADDON_BENCHMARK=ON ../rbdl-rbdl-0879ee8c548a
 make -j
 addons/benchmark/benchmark --floating-base ../../atlas.urdf
