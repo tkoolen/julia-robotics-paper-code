a2e8bd0c8eba00175577b5a4006d6ae86fb03fd7
Twan Koolen <koolen.twan@gmail.com>
2019-02-25 19:14:21 -0500

    Update packages, redo data collection.

--- benchmarks/controller/run.sh
+++ benchmarks/controller/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-julia --project --color=yes -e 'using Pkg; pkg"precompile"'
+julia --project --color=yes -e 'using Pkg; Pkg.instantiate(); pkg"precompile"'
 julia --project -O3 --check-bounds=no --color=yes -e 'using HumanoidLCMSim; AtlasControl.run()' &
-julia --project -O3 --check-bounds=no --color=yes -e 'using HumanoidLCMSim; AtlasSim.run(final_time=10.0)'
+julia --project -O3 --check-bounds=no --color=yes -e 'using LinearAlgebra; BLAS.set_num_threads(1); using HumanoidLCMSim; AtlasSim.run(final_time=10.0)'
 
 # Clean up child process on exit: https://stackoverflow.com/a/2173421/641846
 trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
--- benchmarks/controller/run_lcm_timing.sh
+++ benchmarks/controller/run_lcm_timing.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-julia --project --color=yes -e 'using Pkg; pkg"precompile"; using HumanoidLCMSim'
+julia --project --color=yes -e 'using Pkg; Pkg.instantiate(); pkg"precompile"; using HumanoidLCMSim'
 lcm-logger &
 julia --project -O3 --check-bounds=no --color=yes -e 'using HumanoidLCMSim; AtlasControl.run()' &
-julia --project -O3 --check-bounds=no --color=yes -e 'using HumanoidLCMSim; AtlasSim.run(final_time=10.0, max_rate=1.0, headless=true, controls=false)'
+julia --project -O3 --check-bounds=no --color=yes -e 'using LinearAlgebra; BLAS.set_num_threads(1); using HumanoidLCMSim; AtlasSim.run(final_time=10.0, max_rate=1.0, headless=true, controls=false)'
 
 # Clean up child process on exit: https://stackoverflow.com/a/2173421/641846
-trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
+#trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
